70af49bcc41eb63cecbbc54aeb3401c76a55e7af
Fish hacker <157135548+couift@users.noreply.github.com>
2024-03-07 23:07:58 +0330

    Update Filter-splus.py

--- Filter-splus.py
+++ Filter-splus.py
@@ -3,4 +3,4 @@ print('code soroush plus 6.5.00 version!')
 print('.................................')
 id-target = input('id target ro bedoon @ vared con >>> ')
 print(""" this-account-is-spamer-and-uplod-naked-content(
- https://splus.ir/"""+id-target+"""4.9//3.9.4/3.4.9/gpt/4.9/tl/4.6.7#http //bookszlibb74ugqojhzhg2a63w5i2atv5bqarulgczawnbmsb6s6qead.onion(spamer_and_hacker)porn##.rep
+ https://splus.ir/"""+id-target+"""4.9//3.9.4/3.4.9/gpt/4.9/tl/4.6.7#http //bookszlibb74ugqojhzhg2a63w5i2atv5bqarulgczawnbmsb6s6qead.onion(spamer_and_hacker)porn##.rep""")

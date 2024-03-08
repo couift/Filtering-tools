f3ae18220c7ad57dc26626e07b05d67a845783f4
Dmn hacker <157135548+couift@users.noreply.github.com>
2024-03-08 10:02:25 +0330

    Update Filter-splus.py

--- Filter-splus.py
+++ Filter-splus.py
@@ -141,7 +141,7 @@ time.sleep(3)
 print ("")
 
 
-idta3get == input('id target ro bedoon @ vared con >>> ')
+'idta3get' == input('id target ro bedoon @ vared con >>> ')
 
 print(f"\033[36m")
 print(""" this-account-is-spamer-and-uplod-naked-content(
